165e07886583ef2464c0b9d4b8b0bab684312721
Pavel Boldin <boldin.pavel@gmail.com>
2016-03-13 05:09:12 +0300

    zfsquota_query_many

--- src/zfs.c
+++ src/zfs.c
@@ -14,7 +14,7 @@ int zfsquota_fill_quotadata(void *zfs_handle, struct quota_data *quota_data,
 	int err;
 	uint64_t rid = id;
 
-	quota_data->id = id;
+	quota_data->qid = id;
 
 	err = zfs_userspace_one(zfs_handle,
 				type ==
@@ -59,28 +59,54 @@ no_obj_quota:
 	return 0;
 }
 
-int spam_zfs_quota(struct super_block *zfs_sb)
+typedef int fillinfo_t(uint64_t qid, uint64_t value, void *data);
+
+int zfsquota_query_many(void *zfs_handle, int prop, fillinfo_t cb, void *cbdata)
 {
 	zfs_useracct_t *buf;
 	void *vbuf;
-	int error, i;
-	uint64_t bufsize = sizeof(*buf) * 10;
-	uint64_t cookie = 0;
+	uint64_t bufsize = sizeof(*buf) * 16;
+	uint64_t retsize, i, cookie = 0;
+	int error;
 
-	vbuf = buf = vmem_alloc(bufsize, KM_SLEEP);
-	if (buf == NULL)
+	vbuf = vmem_alloc(bufsize, KM_SLEEP);
+	if (vbuf == NULL)
 		return -ENOMEM;
 
-	error =
-	    zfs_userspace_many(zfs_sb->s_fs_info, ZFS_PROP_USERUSED, &cookie,
-			       buf, &bufsize);
+	do {
+		retsize = bufsize;
+		buf = vbuf;
 
-	for (i = 0; i < bufsize / sizeof(*buf); ++i) {
-		printk("domain = %s, rid = %d, space = %Lu\n", buf->zu_domain,
-		       buf->zu_rid, buf->zu_space);
-		buf++;
-	}
-	vmem_free(vbuf, sizeof(*buf) * 10);
+		error =
+		    zfs_userspace_many(zfs_handle, (zfs_userquota_prop_t) prop,
+				       &cookie, buf, &retsize);
+		if (error)
+			goto out;
 
+		for (i = 0; i < retsize / sizeof(*buf); ++i) {
+			if (cb(buf->zu_rid, buf->zu_space, cbdata))
+				goto out;
+			buf++;
+		}
+	} while (retsize);
+
+out:
+	vmem_free(vbuf, bufsize);
+
+	return error;
+}
+
+static int cb_print_value(uint64_t rid, uint64_t space, void *data)
+{
+	uint64_t *pi = data;
+	printk("rid = %Lu, space = %Lu, *pi = %Lu\n", rid, space, *pi);
+	(*pi)++;
 	return 0;
+}
+
+int spam_zfs_quota(struct super_block *zfs_sb)
+{
+	uint64_t i;
+	return zfsquota_query_many(zfs_sb->s_fs_info, ZFS_PROP_USERUSED,
+				   cb_print_value, &i);
 }
