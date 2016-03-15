6698e4a93a826ee36f0b796364a0427f942db3a1
Pavel Boldin <boldin.pavel@gmail.com>
2016-03-15 22:19:57 +0300

    tools: update install

--- tools/install.sh
+++ tools/install.sh
@@ -3,32 +3,67 @@
 set -x
 set -e
 
+install_openvz() {
+	KERNEL_VERSION="$(rpm -q vzkernel-devel | sed -e '$!d; s/vzkernel-devel-//; s/.x86_64//')"
+	KDIR="/lib/modules/$KERNEL_VERSION/build"
+	export KERNEL_VERSION KDIR
+	[ -d "$KDIR" ] && return
+	[ -f /etc/yum.repos.d/openvz.repo ] || \
+		wget -O /etc/yum.repos.d/openvz.repo \
+			http://download.openvz.org/openvz.repo
+	rpm --import http://download.openvz.org/RPM-GPG-Key-OpenVZ
+	yum install -qy vzkernel vzkernel-devel 
+	KERNEL_VERSION="$(rpm -q vzkernel-devel | sed -e '$!d; s/vzkernel-devel-//; s/.x86_64//')"
+	KDIR="/lib/modules/$KERNEL_VERSION/build"
+	export KERNEL_VERSION KDIR
+	[ -d $KDIR ] || exit 1
+}
+
+install_spl() {
+	[ -f spl_installed ] && return
+	git clone https://github.com/zfsonlinux/spl/ || :
+	pushd spl
+	git pull
+	[ ! -f ./configure ] && ./autogen.sh
+	[ ! -f Makefile ] && ./configure --with-linux-obj=$KDIR
+	make -j
+	make install
+	popd
+	touch spl_installed
+}
+
 install_zfs() {
+	[ -f zfs_installed ] && return
 	git clone https://github.com/paboldin/zfs/ || :
 	pushd zfs
 	git pull
 	git checkout dnode_fix
 	[ ! -f ./configure ] && ./autogen.sh
-	[ ! -f Makefile ] && ./configure
+	[ ! -f Makefile ] && ./configure --with-linux-obj=$KDIR
 	make -j
 	make install
 	popd
+	touch zfs_installed
 }
 
 install_zfs_quota() {
+	[ -f zfs_quota_installed ] && return
 	git clone git@github.com:paboldin/zfs-quota.git || :
 	pushd zfs-quota
 	git pull
-	make
+	make KDIR=$KDIR
 	make install
 	popd
+	touch zfs_quota_installed
 }
 
 fix_initd_vz() {
-	sed -e '/^\s\+MODULES=.*vzdquota/ { ' \
-	       's/#*/#/; p; s/vzdquota \+//; s/#*// }' -i /etc/init.d/vz
+	sed -e '/^\s\+MODULES=.*vzdquota/ {
+	       s/#*/#/; p; s/vzdquota \+//; s/#*// }' -i /etc/init.d/vz
 }
 
+install_openvz
+install_spl
 install_zfs
 install_zfs_quota
 fix_initd_vz
