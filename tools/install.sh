db45955990c9985e83054f0726fb740a433eedc4
Pavel Boldin <boldin.pavel@gmail.com>
2016-03-15 21:43:48 +0300

    tools: install script update

--- tools/install.sh
+++ tools/install.sh
@@ -6,6 +6,7 @@ set -e
 install_zfs() {
 	git clone https://github.com/paboldin/zfs/ || :
 	pushd zfs
+	git pull
 	git checkout dnode_fix
 	[ ! -f ./configure ] && ./autogen.sh
 	[ ! -f Makefile ] && ./configure
@@ -15,11 +16,19 @@ install_zfs() {
 }
 
 install_zfs_quota() {
-	git clone git@github.com:paboldin/zfs-quota.git
+	git clone git@github.com:paboldin/zfs-quota.git || :
 	pushd zfs-quota
+	git pull
 	make
+	make install
 	popd
+}
+
+fix_initd_vz() {
+	sed -e '/^\s\+MODULES=.*vzdquota/ { ' \
+	       's/#*/#/; p; s/vzdquota \+//; s/#*// }' -i /etc/init.d/vz
 }
 
 install_zfs
 install_zfs_quota
+fix_initd_vz
